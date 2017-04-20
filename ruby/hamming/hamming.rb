e948a1ec4a71ebec284f094285cecde989946ea8
lujanfernaud <lujanfernaud@gmail.com>
2017-04-20 18:01:28 +0100

    Implement solution

---
+++ ruby/hamming/hamming.rb
@@ -0,0 +1,19 @@
+module BookKeeping
+  VERSION = 3
+end
+
+class Hamming
+  def self.compute(string_a, string_b)
+    throw ArgumentError unless string_a.length == string_b.length
+
+    strand_a    = string_a.split("")
+    strand_b    = string_b.split("")
+    differences = []
+
+    strand_a.each.with_index do |char_a, index_a|
+      differences << true unless char_a == strand_b[index_a]
+    end
+
+    differences.count
+  end
+end
