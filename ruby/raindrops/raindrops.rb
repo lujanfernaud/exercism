2162a5c36f6312f674c7ebac2832b3f11fc8a00c
lujanfernaud <lujanfernaud@gmail.com>
2017-05-08 22:53:22 +0100

    Start implementation

---
+++ ruby/raindrops/raindrops.rb
@@ -0,0 +1,15 @@
+class Raindrops
+  FACTORS = { 3 => "Pling",
+              5 => "Plang",
+              7 => "Plong" }.freeze
+
+  def self.convert(number)
+    result = ""
+
+    FACTORS.each_key do |factor|
+      result << FACTORS[factor] if (number % factor).zero?
+    end
+
+    result.empty? ? number.to_s : result
+  end
+end
