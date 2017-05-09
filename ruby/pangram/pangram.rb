aed3c57abbdd2a9da56fa190fb7828e9ac5cc67c
lujanfernaud <lujanfernaud@gmail.com>
2017-05-09 16:28:05 +0100

    Do not call to 'chars' on sentence

--- ruby/pangram/pangram.rb
+++ ruby/pangram/pangram.rb
@@ -2,7 +2,7 @@ class Pangram
   ALPHABET = %w[a b c d e f g h i j k l m n o p q r s t v w x y z].freeze
 
   def self.pangram?(sentence)
-    ALPHABET.all? { |char| sentence.downcase.chars.include?(char) }
+    ALPHABET.all? { |char| sentence.downcase.include?(char) }
   end
 end
 
