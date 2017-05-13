34a9763bae6a535a2d576bfa338523789d7a49f0
lujanfernaud <lujanfernaud@gmail.com>
2017-05-13 11:23:41 +0100

    Implement solution for roman_numerals.rb

--- ruby/roman-numerals/roman_numerals.rb
+++ ruby/roman-numerals/roman_numerals.rb
@@ -1,4 +1,19 @@
 class Fixnum
+  ROMAN = { 1 => ["", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"],
+            2 => ["", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"],
+            3 => ["", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"],
+            4 => ["", "M", "MM", "MMM"] }.freeze
+
   def to_roman
+    numeral = to_s
+    places  = [*numeral.length.downto(1)]
+
+    numeral.chars.inject("") do |acc, number|
+      acc + ROMAN[places.shift][number.to_i]
+    end
   end
+end
+
+module BookKeeping
+  VERSION = 2
 end
