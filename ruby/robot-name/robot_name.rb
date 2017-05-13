28f459daad7c6e0903180a034fa8f23d436addb6
lujanfernaud <lujanfernaud@gmail.com>
2017-05-13 15:37:42 +0100

    Implement robot_name.rb

---
+++ ruby/robot-name/robot_name.rb
@@ -0,0 +1,27 @@
+class Robot
+  def self.used_names
+    @used_names ||= []
+  end
+
+  attr_reader :name
+
+  def initialize
+    generate_name
+  end
+
+  def generate_name
+    loop do
+      @name = Array.new(2) { [*"A".."Z"].sample }.join + rand(100..999).to_s
+      break unless Robot.used_names.include?(@name)
+    end
+    Robot.used_names << @name
+  end
+
+  def reset
+    generate_name
+  end
+end
+
+module BookKeeping
+  VERSION = 2
+end
