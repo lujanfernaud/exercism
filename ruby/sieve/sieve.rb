012aa93acab6ee4d69b0722ffd44961a8fb671cc
lujanfernaud <lujanfernaud@gmail.com>
2017-05-13 16:47:45 +0100

    Fourth iteration for sieve.rb
    Do not find multiples, find only primes.

--- ruby/sieve/sieve.rb
+++ ruby/sieve/sieve.rb
@@ -1,24 +1,11 @@
 class Sieve
   def initialize(max_number)
-    @max_number = max_number
-    @numbers    = [*2..max_number]
-    @multiples  = []
-    find_multiples
+    @numbers = [*2..max_number]
   end
 
   def primes
-    @numbers - @multiples
-  end
-
-  def find_multiples
-    2.upto(@max_number - 1) do |number1|
-      next if @multiples.include?(number1)
-
-      2.upto(@max_number) do |number2|
-        multiple = number1 * number2
-        break if multiple > @max_number
-        @multiples << multiple unless @multiples.include?(multiple)
-      end
+    @numbers.each do |num1|
+      @numbers.delete_if { |num2| num2 != num1 && (num2 % num1).zero? }
     end
   end
 end
