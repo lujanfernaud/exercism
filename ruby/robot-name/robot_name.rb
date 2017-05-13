268f48123ee839246f637f968b9beb860286c4d1
lujanfernaud <lujanfernaud@gmail.com>
2017-05-13 15:40:47 +0100

    Second iteration Make #generate_name private

--- ruby/robot-name/robot_name.rb
+++ ruby/robot-name/robot_name.rb
@@ -9,16 +9,18 @@ class Robot
     generate_name
   end
 
+  def reset
+    generate_name
+  end
+
+  private
+
   def generate_name
     loop do
       @name = Array.new(2) { [*"A".."Z"].sample }.join + rand(100..999).to_s
       break unless Robot.used_names.include?(@name)
     end
     Robot.used_names << @name
-  end
-
-  def reset
-    generate_name
   end
 end
 
