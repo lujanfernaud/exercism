cd35c36c524a9e70310a939339362cf49250b410
lujanfernaud <lujanfernaud@gmail.com>
2017-06-30 17:21:08 +0100

    Implement binary.rb

---
+++ ruby/binary/binary.rb
@@ -0,0 +1,39 @@
+# Convert a binary number to its decimal equivalent.
+class Binary
+  NOT_BINARY = /[^0-1]/
+
+  class << self
+    def to_decimal(binary)
+      raise ArgumentError, "#{binary} is not a correct binary number" if
+        binary =~ NOT_BINARY
+
+      return 0 if binary.to_i.zero?
+
+      binary = parse_and_reverse(binary)
+      powers = powers_for_each(binary)
+
+      convert(binary, powers)
+    end
+
+    def parse_and_reverse(binary)
+      binary.each_char.map { |number| number.to_i }.reverse
+    end
+
+    def powers_for_each(binary)
+      binary.map.with_index { |_number, index| 2**index }
+    end
+
+    def convert(binary, powers)
+      binary.map.with_index do |number, index|
+        case number
+        when 0 then 0
+        when 1 then powers[index]
+        end
+      end.inject(:+)
+    end
+  end
+end
+
+module BookKeeping
+  VERSION = 3
+end
