a701878fcd56e825bca7d270d022f06a9e54c6e3
lujanfernaud <lujanfernaud@gmail.com>
2017-09-28 22:34:09 +0100

    Third iteration for grade_school.rb

--- ruby/grade-school/grade_school.rb
+++ ruby/grade-school/grade_school.rb
@@ -1,45 +1,19 @@
 class School
   def initialize
-    @students = []
+    @students = Hash.new([])
   end
 
-  def students(grade_number)
-    store(grade_number)
-    grade.fetch(:students, [])
+  def add(name, grade)
+    (@students[grade] += [name]).sort!
+  end
+
+  def students(grade)
+    @students[grade]
   end
 
   def students_by_grade
-    @students.sort_by { |group| group[:grade] }
-  end
-
-  def add(name, grade_number)
-    store(grade_number)
-    add_name_to_grade(name)
-  end
-
-  private
-
-  def store(grade_number)
-    @grade = grade_number
-  end
-
-  def grade
-    students_by_grade.find { |group| group[:grade] == @grade } || {}
-  end
-
-  def add_name_to_grade(name)
-    add_empty_grade unless grade_exists?
-    (grade[:students] << name).sort!
-  end
-
-  def add_empty_grade
-    @students << { grade: @grade, students: [] }
-  end
-
-  def grade_exists?
-    return false unless grade
-
-    !grade.empty?
+    @students.sort
+             .map { |grade, students| { grade: grade, students: students } }
   end
 end
 
