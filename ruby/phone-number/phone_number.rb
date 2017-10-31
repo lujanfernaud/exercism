e38827df53c524fbcb598b109063e9d90a524f38
lujanfernaud <lujanfernaud@gmail.com>
2017-10-31 22:08:49 +0000

    Second iteration for phone_number.rb

--- ruby/phone-number/phone_number.rb
+++ ruby/phone-number/phone_number.rb
@@ -1,14 +1,11 @@
 class PhoneNumber
   def self.clean(number)
     only_numbers = number.gsub(/\D/, "")
-    valid_phone_number = /^(?:[2-9]\d{2}[2-9]\d{2}\d{4})$|^1(?:[2-9]\d{2}[2-9]\d{2}\d{4})$/
+    valid_phone_number = /^[2-9]\d{2}[2-9]\d{2}\d{4}$|^1[2-9]\d{2}[2-9]\d{2}\d{4}$/
 
     return nil unless only_numbers =~ valid_phone_number
 
-    case only_numbers.length
-    when 10 then only_numbers
-    when 11 then only_numbers[1..-1]
-    end
+    only_numbers.length == 10 ? only_numbers : only_numbers[1..-1]
   end
 end
 
