4e259a123da60872a7827119bf620252e127232f
lujanfernaud <lujanfernaud@gmail.com>
2017-12-20 13:08:31 +0000

    Implement leap.js

---
+++ javascript/leap/leap.js
@@ -0,0 +1,17 @@
+var Year = function (input) {
+  this.input = input;
+};
+
+Year.prototype.isLeap = function () {
+  if (this.input % 4 == 0 && this.input % 400 == 0) {
+    return true;
+  } else if (this.input % 100 == 0) {
+    return false;
+  } else if (this.input % 4 == 0) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+module.exports = Year;
