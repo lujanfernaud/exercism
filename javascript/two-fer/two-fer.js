23749041df7ee2d904685c80dd2063223b9f30f1
lujanfernaud <lujanfernaud@gmail.com>
2017-12-20 09:36:29 +0000

    Implement two-fer.js

---
+++ javascript/two-fer/two-fer.js
@@ -0,0 +1,11 @@
+var TwoFer = function () {};
+
+TwoFer.prototype.twoFer = function (who) {
+  if (who == undefined) {
+    who = "you"
+  }
+
+  return `One for ${who}, one for me.`;
+};
+
+module.exports = TwoFer;
