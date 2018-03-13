dfdef3b3b1078c361590529bd9769077bf1bc66d
lujanfernaud <lujanfernaud@gmail.com>
2018-03-13 22:29:02 +0000

    Fourth iteration for trinary.rb

--- ruby/trinary/trinary.rb
+++ ruby/trinary/trinary.rb
@@ -8,19 +8,8 @@ class Trinary
   def to_decimal
     return 0 unless @trinary =~ VALID
 
-    convert_trinary_string_to_integers_array
-    convert_to_decimal
-  end
-
-  private
-
-  def convert_trinary_string_to_integers_array
-    @trinary = @trinary.split("").map(&:to_i)
-  end
-
-  def convert_to_decimal
-    @trinary.reverse.map.with_index.sum do |number, index|
-      number * 3**index
+    @trinary.reverse.each_char.with_index.sum do |number, index|
+      number.to_i * 3**index
     end
   end
 end
