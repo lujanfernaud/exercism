82e90e26c8b56927077cd3ff2bc4b4dfe0478fd4
lujanfernaud <lujanfernaud@gmail.com>
2018-03-31 15:11:19 +0100

    Seventh iteration for beer-song.rb

--- ruby/beer-song/verse.rb
+++ ruby/beer-song/verse.rb
@@ -11,30 +11,24 @@ class Verse
 
   def verse_with_bottles
     <<~VERSE
-      #{number} #{containers} of beer on the wall, #{number} #{containers} of beer.
-      Take #{pronoun} down and pass it around, #{number &minus_one} #{containers} of beer on the wall.
+      #{quantity} #{containers} of beer on the wall, #{quantity} #{containers} of beer.
+      Take #{pronoun} down and pass it around, #{quantity minus_one} #{containers minus_one} of beer on the wall.
     VERSE
   end
 
   def verse_without_bottles
     <<~VERSE
-      #{number.capitalize} #{containers} of beer on the wall, #{number} #{containers} of beer.
+      #{quantity.capitalize} #{containers} of beer on the wall, #{quantity} #{containers} of beer.
       Go to the store and buy some more, 99 bottles of beer on the wall.
     VERSE
   end
 
-  def number
-    @number = yield(@number) if block_given?
-
-    @number == 0 ? "no more" : @number
+  def quantity(number = @number)
+    number == 0 ? "no more" : number
   end
 
-  def minus_one
-    proc { |number| number - 1 }
-  end
-
-  def containers
-    @number == 1 ? "bottle" : "bottles"
+  def containers(number = @number)
+    number == 1 ? "bottle" : "bottles"
   end
 
   def pronoun
@@ -42,5 +36,9 @@ class Verse
     when 2..99 then "one"
     when 1     then "it"
     end
+  end
+
+  def minus_one
+    @number - 1
   end
 end
