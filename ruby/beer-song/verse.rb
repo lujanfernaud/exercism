95a723f6055cc581bb2bd2544425a02efb04465d
lujanfernaud <lujanfernaud@gmail.com>
2018-03-31 16:07:27 +0100

    Eleventh iteration for beer-song.rb

--- ruby/beer-song/verse.rb
+++ ruby/beer-song/verse.rb
@@ -4,50 +4,53 @@ class Verse
   end
 
   def build
-    @number > 0 ? verse_with_bottles : verse_without_bottles
-  end
-
-  private
-
-  def verse_with_bottles
     <<~VERSE
-      #{containers_of_beverage} on the wall, #{containers_of_beverage}.
+      #{containers_of_beverage.capitalize} on the wall, #{containers_of_beverage}.
       #{action}, #{containers_of_beverage minus_one} on the wall.
     VERSE
   end
 
-  def verse_without_bottles
-    <<~VERSE
-      #{containers_of_beverage.capitalize} on the wall, #{containers_of_beverage}.
-      #{action}, #{containers_of_beverage(99)} on the wall.
-    VERSE
-  end
+  private
 
   def containers_of_beverage(number = @number)
     "#{quantity(number)} #{containers(number)} of beer"
   end
 
   def quantity(number)
-    number == 0 ? "no more" : number.to_s
+    if number == 0
+      "no more"
+    elsif number == -1
+      "99"
+    else
+      number.to_s
+    end
   end
 
   def containers(number)
-    number == 1 ? "bottle" : "bottles"
+    if number == 1
+      "bottle"
+    else
+      "bottles"
+    end
   end
 
-  def minus_one
-    @number - 1
-  end
-
-  def action(number = @number)
-    if number == 0
+  def action
+    if @number == 0
       "Go to the store and buy some more"
     else
       "Take #{pronoun} down and pass it around"
     end
   end
 
-  def pronoun(number = @number)
-    number == 1 ? "it" : "one"
+  def pronoun
+    if @number == 1
+      "it"
+    else
+      "one"
+    end
+  end
+
+  def minus_one
+    @number - 1
   end
 end
