fa4299291adb4a4adedf5baa6f495c9e1981df1d
lujanfernaud <lujanfernaud@gmail.com>
2019-03-08 06:53:55 +0000

    Fifth iteration for matrix.rb

--- ruby/matrix/matrix.rb
+++ ruby/matrix/matrix.rb
@@ -8,27 +8,13 @@ class Matrix
   end
 
   def columns
-    [].tap do |columns|
-      columns_number.times do |column_index|
-        columns << items_in(column_index)
-      end
-    end
+    rows.transpose
   end
 
   private
 
   def individual_rows
     proc { |item| item.split.map(&:to_i) }
-  end
-
-  def columns_number
-    rows.map(&:length).max
-  end
-
-  def items_in(column_index)
-    rows.each_with_object([]) do |row, column|
-      column << row[column_index]
-    end
   end
 
   attr_reader :matrix
