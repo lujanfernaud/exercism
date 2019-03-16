6bdb37bf7166bbd5a354262b8130b402a689ce49
lujanfernaud <lujanfernaud@gmail.com>
2019-03-16 22:44:41 +0000

    Second iteration for flatten_array.rb

--- ruby/flatten-array/flatten_array.rb
+++ ruby/flatten-array/flatten_array.rb
@@ -1,5 +1,31 @@
 class FlattenArray
   def self.flatten(array)
-    array.flatten.compact
+    new(array).call
   end
+
+  def initialize(array)
+    @array = array
+    @new_array = []
+  end
+
+  def call
+    extract_values_from(array)
+    new_array
+  end
+
+  private
+
+  def extract_values_from(array)
+    array.each do |value|
+      next if value.nil?
+
+      if value.is_a?(Array)
+        extract_values_from(value)
+      else
+        @new_array << value
+      end
+    end
+  end
+
+  attr_reader :array, :new_array
 end
