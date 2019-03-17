4bdb8e8134f552b0ecf9c74089619f9e122c6b9a
lujanfernaud <lujanfernaud@gmail.com>
2019-03-17 09:00:40 +0000

    Second iteration for triangle.rb

--- ruby/triangle/triangle.rb
+++ ruby/triangle/triangle.rb
@@ -4,32 +4,40 @@ class Triangle
   end
 
   def equilateral?
-    return false if unique_side_lengths.first.zero?
-
-    sides_with_same_length == 1
+    valid? && sides_with_same_length == 1
   end
 
   def isosceles?
-    return false if unique_side_lengths.include?(1)
-
-    sides_with_same_length == 1 || sides_with_same_length == 2
+    valid? && (sides_with_same_length == 1 || sides_with_same_length == 2)
   end
 
   def scalene?
-    sorted_side_lengths = triangle.max(3)
-    longer_side = sorted_side_lengths.shift
-
-    longer_side <= sorted_side_lengths.sum && !equilateral? && !isosceles?
+    valid? && !equilateral? && !isosceles?
   end
 
   private
 
-  def sides_with_same_length
-    unique_side_lengths.size
+  def valid?
+    !sides_too_short? && valid_proportions?
+  end
+
+  def sides_too_short?
+    !(unique_side_lengths & [0, 1]).empty?
   end
 
   def unique_side_lengths
-    @unique_side_lengths ||= @triangle.uniq
+    @unique_side_lengths ||= triangle.uniq
+  end
+
+  def valid_proportions?
+    sorted_side_lengths = triangle.max(3)
+    longer_side = sorted_side_lengths.shift
+
+    longer_side <= sorted_side_lengths.sum
+  end
+
+  def sides_with_same_length
+    @sides_with_same_length ||= unique_side_lengths.size
   end
 
   attr_reader :triangle
