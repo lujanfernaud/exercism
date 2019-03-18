9b0bba0de38a59db250fed752ead008b354c6f3c
lujanfernaud <lujanfernaud@gmail.com>
2019-03-18 21:12:53 +0000

    Third iteration for isogram.rb

--- ruby/isogram/isogram.rb
+++ ruby/isogram/isogram.rb
@@ -1,9 +1,9 @@
 class Isogram
   def self.isogram?(string)
-    string.downcase!
-
     return true if string.empty?
 
-    string.scan(/\w/).uniq == string.scan(/\w/)
+    characters = string.downcase.scan(/\w/)
+
+    characters.uniq == characters
   end
 end
