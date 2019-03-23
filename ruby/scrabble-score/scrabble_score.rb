acead147fdf943b920b276fdc31e687433d81f18
lujanfernaud <lujanfernaud@gmail.com>
2019-03-23 12:44:32 +0000

    First iteration for scrabble_score.rb

---
+++ ruby/scrabble-score/scrabble_score.rb
@@ -0,0 +1,37 @@
+class Scrabble
+  SCORES = {
+    1  => %w[A E I O U L N R S T],
+    2  => %w[D G],
+    3  => %w[B C M P],
+    4  => %w[F H V W Y],
+    5  => %w[K],
+    8  => %w[J X],
+    10 => %w[Q Z]
+  }
+
+  def self.score(word)
+    new(word).score
+  end
+
+  def initialize(word)
+    @word = word
+  end
+
+  def score
+    return 0 if word.nil?
+
+    characters.map(&method(:to_score)).sum
+  end
+
+  private
+
+  def characters
+    word.upcase.scan(/\w/)
+  end
+
+  def to_score(character)
+    SCORES.find { |_key, value| value.include?(character) }.first
+  end
+
+  attr_reader :word
+end
