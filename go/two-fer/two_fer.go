9812304e40d180a5d9b104d5415b0f2dd912304e
Luján Fernaud <lujanfernaud@gmail.com>
2019-07-22 22:38:40 +0100

    Third iteration for two_fer.go

--- go/two-fer/two_fer.go
+++ go/two-fer/two_fer.go
@@ -1,9 +1,12 @@
+// Package twofer provides solution for "two for one" Exercism's problem.
 package twofer
 
 import (
 	"fmt"
 )
 
+// ShareWith returns sentence with given name, or with "you" is no name is
+// provided.
 func ShareWith(name string) string {
 	if name == "" {
 		name = "you"
