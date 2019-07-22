6e29720f800cd3a9bf651a3cf74f255797636811
Luján Fernaud <lujanfernaud@gmail.com>
2019-07-22 22:32:23 +0100

    Second iteration for two_fer.go

--- go/two-fer/two_fer.go
+++ go/two-fer/two_fer.go
@@ -5,13 +5,9 @@ import (
 )
 
 func ShareWith(name string) string {
-	var name_placeholder string
-
-	if len(name) == 0 {
-		name_placeholder = "you"
-	} else {
-		name_placeholder = name
+	if name == "" {
+		name = "you"
 	}
 
-	return fmt.Sprintf("One for %s, one for me.", name_placeholder)
+	return fmt.Sprintf("One for %s, one for me.", name)
 }
