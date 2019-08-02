68d92d1da437e5be7d50184383b05e5ddb7b10f9
Luján Fernaud <lujanfernaud@gmail.com>
2019-08-02 18:26:54 +0100

    Fourth iteration for triangle.go

--- go/triangle/triangle.go
+++ go/triangle/triangle.go
@@ -18,20 +18,26 @@ const (
 
 // KindFromSides returns the type of triangle depending on the sizes of the sides.
 func KindFromSides(a, b, c float64) Kind {
-	values := []float64{a, b, c}
-	sort.Float64s(values)
-
-	if incorrectValues(values) {
+	if incorrectValues(a, b, c) {
 		return NaT
 	}
 
-	uniqueSides := calculateUniqueSides(values)
+	if a == b && b == c {
+		return Equ
+	}
 
-	return triangleKind(uniqueSides)
+	if a == b || b == c || a == c {
+		return Iso
+	}
+
+	return Sca
 }
 
-func incorrectValues(values []float64) bool {
-	if len(values) == 3 && values[0]+values[1] < values[2] {
+func incorrectValues(a, b, c float64) bool {
+	values := []float64{a, b, c}
+	sort.Float64s(values)
+
+	if values[0]+values[1] < values[2] {
 		return true
 	}
 
@@ -42,37 +48,4 @@ func incorrectValues(values []float64) bool {
 	}
 
 	return false
-}
-
-func calculateUniqueSides(values []float64) int {
-	lengths := map[float64]bool{}
-
-	for _, value := range values {
-		lengths[value] = true
-	}
-
-	uniqueSides := []float64{}
-
-	for key := range lengths {
-		uniqueSides = append(uniqueSides, key)
-	}
-
-	return len(uniqueSides)
-}
-
-func triangleKind(uniqueSides int) Kind {
-	var kind Kind
-
-	switch uniqueSides {
-	case 1:
-		kind = Equ
-	case 2:
-		kind = Iso
-	case 3:
-		kind = Sca
-	default:
-		kind = NaT
-	}
-
-	return kind
 }
