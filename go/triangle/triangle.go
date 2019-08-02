d50e4692e21a36f948ca59adc36e3cdf1127fdaf
Luján Fernaud <lujanfernaud@gmail.com>
2019-08-02 17:43:10 +0100

    Second iteration for triangle.go

--- go/triangle/triangle.go
+++ go/triangle/triangle.go
@@ -18,18 +18,58 @@ const (
 
 // KindFromSides returns the type of triangle depending on the sizes of the sides.
 func KindFromSides(a, b, c float64) Kind {
-	sides := []float64{a, b, c}
-	sort.Float64s(sides)
+	values := []float64{a, b, c}
+	sort.Float64s(values)
 
-	if incorrectValues(sides) {
+	if incorrectValues(values) {
 		return NaT
 	}
 
-	result := sidesWithTheSameLenght(sides)
+	uniqueSides := sidesWithTheSameLength(values)
 
+	return triangleKind(uniqueSides)
+}
+
+func incorrectValues(values []float64) bool {
+	if values[0] == 0 || values[1] == 0 || values[2] == 0 {
+		return true
+	}
+
+	if len(values) == 3 && values[0]+values[1] < values[2] {
+		return true
+	}
+
+	if math.IsNaN(values[0]) || math.IsNaN(values[1]) || math.IsNaN(values[2]) {
+		return true
+	}
+
+	if math.IsInf(values[0], 0) || math.IsInf(values[1], 0) || math.IsInf(values[2], 0) {
+		return true
+	}
+
+	return false
+}
+
+func sidesWithTheSameLength(values []float64) int {
+	lengths := map[float64]bool{}
+
+	for _, value := range values {
+		lengths[value] = true
+	}
+
+	uniqueSides := []float64{}
+
+	for key := range lengths {
+		uniqueSides = append(uniqueSides, key)
+	}
+
+	return len(uniqueSides)
+}
+
+func triangleKind(uniqueSides int) Kind {
 	var kind Kind
 
-	switch len(result) {
+	switch uniqueSides {
 	case 1:
 		kind = Equ
 	case 2:
@@ -41,40 +81,4 @@ func KindFromSides(a, b, c float64) Kind {
 	}
 
 	return kind
-}
-
-func incorrectValues(sides []float64) bool {
-	if sides[0] == 0 || sides[1] == 0 || sides[2] == 0 {
-		return true
-	}
-
-	if len(sides) == 3 && sides[0]+sides[1] < sides[2] {
-		return true
-	}
-
-	if math.IsNaN(sides[0]) || math.IsNaN(sides[1]) || math.IsNaN(sides[2]) {
-		return true
-	}
-
-	if math.IsInf(sides[0], 0) || math.IsInf(sides[1], 0) || math.IsInf(sides[2], 0) {
-		return true
-	}
-
-	return false
-}
-
-func sidesWithTheSameLenght(sides []float64) []float64 {
-	lengths := map[float64]bool{}
-
-	for _, side := range sides {
-		lengths[side] = true
-	}
-
-	result := []float64{}
-
-	for key := range lengths {
-		result = append(result, key)
-	}
-
-	return result
 }
