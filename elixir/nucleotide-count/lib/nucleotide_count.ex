172d5d9efee92d38432a3802596a9e903ec3a94f
Luján Fernaud <lujanfernaud@gmail.com>
2020-04-10 08:10:44 +0100

    Fourth iteration for nucleotide_count.ex

--- elixir/nucleotide-count/lib/nucleotide_count.ex
+++ elixir/nucleotide-count/lib/nucleotide_count.ex
@@ -14,7 +14,7 @@ defmodule NucleotideCount do
   """
   @spec count(charlist(), char()) :: non_neg_integer()
   def count(strand, nucleotide) do
-    strand |> Enum.count(&(&1 == nucleotide))
+    Enum.count(strand, &(&1 == nucleotide))
   end
 
   @doc """
