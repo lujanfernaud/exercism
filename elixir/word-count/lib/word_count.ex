9bdb1e35c4152a30a6966db0bead3cf4304bcc31
Luján Fernaud <lujanfernaud@gmail.com>
2020-04-12 08:29:42 +0100

    Second iteration for word_count.ex

--- elixir/word-count/lib/word_count.ex
+++ elixir/word-count/lib/word_count.ex
@@ -1,4 +1,6 @@
 defmodule WordCount do
+  @non_word_characters_regexp ~r/[^-a-z0-9\p{L}]|^_/u
+
   @doc """
   Count the number of words in the sentence.
 
@@ -8,7 +10,7 @@ defmodule WordCount do
   def count(sentence) do
     sentence
     |> String.downcase()
-    |> String.replace(~r/[^-a-zA-Z0-9\p{L}]|^_/u, " ")
+    |> String.replace(@non_word_characters_regexp, " ")
     |> String.split()
     |> Enum.frequencies()
   end
