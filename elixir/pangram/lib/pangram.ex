8f4a7fea3ddafe24b2f2fc6fd902428ebd4ca9c5
Luján Fernaud <lujanfernaud@gmail.com>
2020-04-26 13:19:52 +0100

    Third iteration for pangram.ex

--- elixir/pangram/lib/pangram.ex
+++ elixir/pangram/lib/pangram.ex
@@ -1,18 +1,18 @@
 defmodule Pangram do
-  @alphabet Enum.to_list(?a..?z)
-
   @doc """
   Determines if a word or sentence is a pangram.
   A pangram is a sentence using every letter of the alphabet at least once.
 
   Returns a boolean.
 
-    ## Examples
+  ## Examples
 
-      iex> Pangram.pangram?("the quick brown fox jumps over the lazy dog")
-      true
+  iex> Pangram.pangram?("the quick brown fox jumps over the lazy dog")
+  true
 
   """
+
+  @alphabet Enum.to_list(?a..?z)
 
   @spec pangram?(String.t()) :: boolean
   def pangram?(sentence) do
