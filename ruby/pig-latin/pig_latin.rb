88152998d6535dbc9f237757dd8a01b2d1533318
Luján Fernaud <lujanfernaud@gmail.com>
2020-05-29 14:20:20 +0100

    Fifth iteration for pig_latin.rb

--- ruby/pig-latin/pig_latin.rb
+++ ruby/pig-latin/pig_latin.rb
@@ -1,8 +1,9 @@
 class PigLatin
-  VOWEL_SOUNDS_REGEX = /\A(xr|xt|yt|a|e|i|o|u)(.*)/.freeze
+  VOWEL_SOUNDS_REGEX = /\A(xr|xt|yt|a|e|i|o|u)/.freeze
 
   CONSONANT_SOUNDS_REGEX = /
-    \A(squ|thr|sch|ch|rh|qu|th|b|c|d|f|g|h|j|k|l|m|n|p|q|r|s|t|v|w|x|y|z)(.*)
+    (?<consonant>squ|thr|sch|ch|rh|qu|th|b|c|d|f|g|h|j|k|l|m|n|p|q|r|s|t|v|w|x|y|z)
+    (?<rest>.*)
   /x.freeze
 
   class << self
@@ -15,9 +16,7 @@ class PigLatin
     def translate_word(word)
       return word + "ay" if word.match(VOWEL_SOUNDS_REGEX)
 
-      _word, consonant_sound, rest = word.match(CONSONANT_SOUNDS_REGEX).to_a
-
-      rest + consonant_sound + "ay"
+      word.sub(CONSONANT_SOUNDS_REGEX, '\k<rest>\k<consonant>ay')
     end
   end
 end
