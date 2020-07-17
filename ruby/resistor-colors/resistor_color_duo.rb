61a9050f6b7f2e1a50a50012726ff5a618c3450a
Luján Fernaud <lujanfernaud@gmail.com>
2020-07-17 09:46:50 +0100

    Third iteration for resistor_color_duo.rb

---
+++ ruby/resistor-colors/resistor_color_duo.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+class ResistorColorDuo
+  COLOR_CODES = {
+    "black" => 0,
+    "brown" => 1,
+    "red" => 2,
+    "orange" => 3,
+    "yellow" => 4,
+    "green" => 5,
+    "blue" => 6,
+    "violet" => 7,
+    "grey" => 8,
+    "white" => 9
+  }.freeze
+
+  def self.value(colors)
+    colors.first(2).map(&COLOR_CODES).join.to_i
+  end
+end
