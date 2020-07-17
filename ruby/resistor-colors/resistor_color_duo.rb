18bbe7ee8964fee2dbeee1763cea769d3d47e53c
Luján Fernaud <lujanfernaud@gmail.com>
2020-07-17 09:48:06 +0100

    Fourth iteration for resistor_color_duo.rb

--- ruby/resistor-colors/resistor_color_duo.rb
+++ ruby/resistor-colors/resistor_color_duo.rb
@@ -2,16 +2,16 @@
 
 class ResistorColorDuo
   COLOR_CODES = {
-    "black" => 0,
-    "brown" => 1,
-    "red" => 2,
+    "black"  => 0,
+    "brown"  => 1,
+    "red"    => 2,
     "orange" => 3,
     "yellow" => 4,
-    "green" => 5,
-    "blue" => 6,
+    "green"  => 5,
+    "blue"   => 6,
     "violet" => 7,
-    "grey" => 8,
-    "white" => 9
+    "grey"   => 8,
+    "white"  => 9
   }.freeze
 
   def self.value(colors)
