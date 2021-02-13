ef3bc74ce790f2329084af2b1c70e687a93924b9
Luján Fernaud <lujanfernaud@gmail.com>
2021-02-13 11:26:35 +0000

    Sixth iteration for pig_latin.rb

--- ruby/pig-latin/pig_latin.rb
+++ ruby/pig-latin/pig_latin.rb
@@ -8,13 +8,13 @@ class PigLatin
 
   class << self
     def translate(sentence)
-      sentence.split.map(&method(:translate_word)).join(" ")
+      sentence.split.map { |word| translate_word(word) }.join(" ")
     end
 
     private
 
     def translate_word(word)
-      return word + "ay" if word.match(VOWEL_SOUNDS_REGEX)
+      return "#{word}ay" if word.match(VOWEL_SOUNDS_REGEX)
 
       word.sub(CONSONANT_SOUNDS_REGEX, '\k<rest>\k<consonant>ay')
     end
