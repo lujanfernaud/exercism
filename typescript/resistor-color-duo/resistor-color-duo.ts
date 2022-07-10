9e3f77b54d9fc55409bedd02d4a153b29c9cc238
Luján Fernaud <lujanfernaud@gmail.com>
2022-07-10 10:05:03 +0100

    Fifth iteration for resistor-color-duo.ts

--- typescript/resistor-color-duo/resistor-color-duo.ts
+++ typescript/resistor-color-duo/resistor-color-duo.ts
@@ -1,20 +1,16 @@
-const COLOR_TO_VALUE: Record<string, number> = {
-  "black": 0,
-  "brown": 1,
-  "red": 2,
-  "orange": 3,
-  "yellow": 4,
-  "green": 5,
-  "blue": 6,
-  "violet": 7,
-  "grey": 8,
-  "white": 9,
-}
+const COLORS = [
+  "black",
+  "brown",
+  "red",
+  "orange",
+  "yellow",
+  "green",
+  "blue",
+  "violet",
+  "grey",
+  "white"
+]
 
-export function decodedValue(colors: Array<string>): number {
-  const result = colors.slice(0, 2).reduce((result: string, color: string): string => {
-    return result + COLOR_TO_VALUE[color]
-  }, "")
-
-  return Number(result)
+export function decodedValue([color1, color2]: typeof COLORS): number {
+  return Number(`${COLORS.indexOf(color1)}${COLORS.indexOf(color2)}`)
 }
