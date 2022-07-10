2d8fed8f1a67ee3dbb5a802f56549bad513eb074
Luján Fernaud <lujanfernaud@gmail.com>
2022-07-10 10:13:10 +0100

    Sixth iteration for resistor-color-duo.ts

--- typescript/resistor-color-duo/resistor-color-duo.ts
+++ typescript/resistor-color-duo/resistor-color-duo.ts
@@ -1,16 +1,16 @@
-const COLORS = [
-  "black",
-  "brown",
-  "red",
-  "orange",
-  "yellow",
-  "green",
-  "blue",
-  "violet",
-  "grey",
-  "white"
-]
+const COLOR_CODES = {
+  "black": 0,
+  "brown": 1,
+  "red": 2,
+  "orange": 3,
+  "yellow": 4,
+  "green": 5,
+  "blue": 6,
+  "violet": 7,
+  "grey": 8,
+  "white": 9,
+}
 
-export function decodedValue([color1, color2]: typeof COLORS): number {
-  return Number(`${COLORS.indexOf(color1)}${COLORS.indexOf(color2)}`)
+export function decodedValue([firstColor, secondColor]: Array<keyof typeof COLOR_CODES>): number {
+  return Number(`${COLOR_CODES[firstColor]}${COLOR_CODES[secondColor]}`)
 }
